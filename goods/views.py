9d4981ae0adad5f640a1f00aa2aed1babb878ae7
luckypupil <blakesadams@gmail.com>
2014-12-16 13:32:53 -0500

    Updated req.txt file, updated styling for homepage and detail pages

--- goods/views.py
+++ goods/views.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
 from goods.models import Product
+from merchants.models import Store
 
 class HomeView(generic.ListView):
 	template_name = 'goods/home/home.html'
@@ -14,3 +15,8 @@ class DetailView(generic.DetailView):
 	template_name = 'goods/detail/detail.html'
 	context_object_name = 'product'
 	model = Product
+
+class MapView(generic.ListView):
+	template_name = 'goods/map/map.html'
+	context_object_name = 'stores'
+	model = Store
