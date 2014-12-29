8d171d7eec11153751d06de2274ab49b57190478
luckypupil <blakesadams@gmail.com>
2014-12-28 19:53:47 -0500

    Updated login with modal.  Changed logout view to logout_then_login

--- members/urls.py
+++ members/urls.py
@@ -6,6 +6,6 @@ from members.forms import CustomAuthenticationForm
 urlpatterns = patterns('',
     url(r'^account/(?P<pk>\d+)$', views.ClosetView.as_view(), name='closet'),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'members/auth/login.html', 'authentication_form' : CustomAuthenticationForm}, name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'members/auth/login.html'}, name='logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
 
 )
