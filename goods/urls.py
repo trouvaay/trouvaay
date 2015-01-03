399331618e71371c15a0b9cae9ffc5a8af406d4d
luckypupil <blakesadams@gmail.com>
2015-01-03 17:45:49 -0500

    url rename

--- goods/urls.py
+++ goods/urls.py
@@ -3,7 +3,7 @@ from goods import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^$', views.HomeView.as_view(), name='new'),
     url(r'^(?P<pk>\d+)/$', views.DetailRouteView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/map/$', views.MapView.as_view(), name='popinmap'),
     url(r'^nearby/$', views.NearbyView.as_view(), name='nearby'),
