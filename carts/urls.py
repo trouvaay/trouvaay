51ffcf3ad92385398ce6d4efe86c3ff4049fbdbf
luckypupil <blakesadams@gmail.com>
2015-01-08 19:34:18 -0800

    removed checkout,carts folders

--- carts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from carts import views
-
-
-# urlpatterns = patterns('',
-#     url(r'^cart/$', views.AddCartProduct, name='addtocart'),
-# )
--- checkout/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-from checkout import views
-
-urlpatterns = patterns('',
-    url(r'^stripe/$', views.StripeView, name='stripe'),
-)
