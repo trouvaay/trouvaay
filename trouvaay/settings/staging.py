a28658a50e88cbd5627820a419e1d570f64a97b3
luckypupil <blakesadams@gmail.com>
2015-01-20 11:38:14 -0800

    email credentials updated for staging settings

--- trouvaay/settings/staging.py
+++ trouvaay/settings/staging.py
@@ -1,4 +1,5 @@
 from .base import *
+import os
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
@@ -15,6 +16,12 @@ STATIC_ROOT = 'staticfiles'
 SECRET_KEY = os.getenv('SECRET_KEY', None)
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', None)
 STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY', None)
+
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', None)
+EMAIL_HOST = os.getenv('EMAIL_HOST', None) # 'smtp.gmail.com'
+EMAIL_PORT =  os.getenv('EMAIL_PORT', None) # 587 - for gmail
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', None) # e.g. 'support@raredoor.com'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', None)
 
 INSTALLED_APPS = (
     'django.contrib.admin',
