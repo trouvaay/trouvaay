4f03d3aec53b25f405a4b99c663316a96d464b11
luckypupil <blakesadams@gmail.com>
2015-01-28 21:41:27 -0800

    updated base.py

--- trouvaay/settings/base.py
+++ trouvaay/settings/base.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sslify
 from decimal import Decimal
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
@@ -29,7 +30,7 @@ TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = [ ]
 
 # works with sslify to force https on Heroku
-SSLIFY_DISABLE = True
+
 
 INSTALLED_APPS = (
     'django.contrib.admin',
