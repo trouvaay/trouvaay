afe24561b50881c2465d6a72926f812093fa18a6
luckypupil <blakesadams@gmail.com>
2015-02-06 17:33:50 -0800

    cleaned up goog_upload

--- goods/management/commands/goog_download.py
+++ goods/management/commands/goog_download.py
@@ -83,57 +83,5 @@ class Command(BaseCommand):
                 logger.error(str(e))
             
 
-            
-            
-
-            
-
-            # add manytomany fields
-            
-
-
-
-            
-
-            # Store = data[0]
-            # store_id = int(data[1])
-            # short_name = unicode(data [2]) # for slugify
-            # url = data[3]
-            # is_sold = (True if data[4] == 'True' else False)
-            # original_price = float(data[5])
-            # current_price = float(data[6])
-            # is_custom = (True if data[7] == 'True' else False)
-            # is_floor_model = (True if data[8] == 'True' else False)
-            # description = data[9]
-            # width = (None if data[10] == None else float(data[10]))
-            # depth = (None if data[11] == None else float(data[11]))
-            # height = (None if data[12] == None else float(data[12]))
-            # seat_height = (None if data[13] == None else float(data[13]))
-            # diameter = (None if data[14] == None else float(data[14]))
-            # bed_size = data[15]
-            # weight = (None if data[16] == None else float(data[16]))
-            # color1 = data[17]
-            # color2 = data[18]
-            # color_description = data[19]
-            # material = data[20]
-            # material_description = data[21]
-            # segment = Segment.objects.get(select=data[22])
-            # furnituretype = FurnitureType.objects.get(select=data[23])
-            # subcatergory = Subcategory.objects.get(select=data[24])
-            # delivery_weeks = data[25]
-            # main_image_url = data[26]
-            # image_2_url = data[27]
-            # image_3_url = data[28]
-            # image_4_url = data[29]
-
-            # u = Product()
-            # u.store_id = store_id
-            # u.short_name = short_name
-            # u.original_price = original_price
-            # u.current_price = current_price
-            # u.save()
-            
-        # except:
-        #     self.stderr.write('looks like this given row is out of range')
         
         
