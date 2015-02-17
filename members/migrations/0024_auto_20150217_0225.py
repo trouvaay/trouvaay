d82356b48a4d72fab482e0e7e626699de0f03fc9
luckypupil <blakesadams@gmail.com>
2015-02-17 00:06:48 -0800

    migrations conflict dependinceies updated

--- members/migrations/0024_auto_20150217_0225.py
+++ members/migrations/0024_auto_20150217_0225.py
@@ -8,7 +8,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('members', '0022_auto_20150211_2048'),
+        ('members', '0023_authuserorderitem_has_open_reservation'),
     ]
 
     operations = [
