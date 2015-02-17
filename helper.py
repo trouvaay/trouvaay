e3ad9974f79023cbd623b7e370812713dba85f3e
Sergey Golovchenko <sergey@lanasoft.net>
2015-02-17 11:05:19 -0500

    updated send_email_from_template to send copy to ourselves as bcc

--- helper.py
+++ helper.py
@@ -56,8 +56,12 @@ def send_email_from_template(to_email, context, subject_template, plain_text_bod
 	subject = ''.join(subject.splitlines()) # remove new lines from subject
 	
 	message_txt = render_to_string(plain_text_body_template, context)
-	email_message = EmailMultiAlternatives(subject, message_txt, settings.DEFAULT_FROM_EMAIL, [to_email, settings.DEFAULT_FROM_EMAIL])
-	
+	email_message = EmailMultiAlternatives(subject=subject, 
+                                           body=message_txt, 
+                                           from_email=settings.DEFAULT_FROM_EMAIL, 
+                                           to=[to_email], 
+                                           bcc=[settings.DEFAULT_FROM_EMAIL])
+    
 	if(html_body_template):
 		message_html = render_to_string(html_body_template, context)
 		email_message.attach_alternative(message_html, 'text/html')
