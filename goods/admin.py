20ce13763ba3885b17ede54593df87e49f9d29a6
luckypupil <blakesadams@gmail.com>
2015-03-05 10:17:09 -0500

    updated formatting for Profile page product labels and stuck footer to bottom of about us page

--- goods/admin.py
+++ goods/admin.py
@@ -35,14 +35,14 @@ class ProductImageAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     model = Product
 
-    list_display = ['short_name', 'first_image', 'store', 'click_count', 'is_published', 'is_featured', 'is_reserved', 'is_sold', 'current_price', 'added_date', 'pub_date', 'is_landing', 'is_featured',]
+    list_display = ['short_name', 'id', 'first_image', 'store', 'click_count', 'is_published', 'is_featured', 'is_reserved', 'is_sold', 'current_price', 'added_date', 'pub_date', 'is_landing', 'is_featured',]
 
     fields = [('short_name', 'is_published', 'is_sold', 'is_featured'), ('store', 'has_trial', 'units'),('original_price', 'current_price'), 
                 'pub_date', 'description',('color', 'color_description'),('style', 'segment', 'furnituretype', 'category', 'subcategory', 'material'),
                 'width', 'depth', 'height', 'seat_height', 'diameter', 'bed_size']
     inlines = [ProductImageInline, AuthUserOrderItemInline]
 
-    search_fields = ['short_name', 'long_name', 'store']
+    search_fields = ['short_name', 'id']
     list_filter = ['store', 'is_published', 'furnituretype', 'is_landing']
 
     list_editable = ['current_price', 'is_published','is_landing', 'is_reserved', 'is_sold', 'is_featured', 'pub_date']
