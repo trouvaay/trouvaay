f33d3e1e95922d394e798f9c6ca5d760deb832c4
luckypupil <blakesadams@gmail.com>
2015-04-26 12:00:28 -0400

    removed sites app from installed apps

--- trouvaay/settings/staging.py
+++ trouvaay/settings/staging.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
     'localflavor',
     'merchants',
     'members',
