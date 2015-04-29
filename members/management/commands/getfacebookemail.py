725992ff5ce414c21959a5d24aae9ad7b600eb3b
luckypupil <blakesadams@gmail.com>
2015-04-29 10:49:17 -0700

    updated getfbemail command

--- members/management/commands/getfacebookemail.py
+++ members/management/commands/getfacebookemail.py
@@ -82,7 +82,7 @@ class Command(BaseCommand):
             if(not user.email or '@' not in user.email):
                 # user is missing email
                 
-                print 'getting email for {}'.formate(user)
+                print 'getting email for {}'.format(user)
 
                 email = get_facebook_email(user)
                 if(email):
