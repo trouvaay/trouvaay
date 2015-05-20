56214b31387d344809e6a484beeb1a0038864eed
luckypupil <blakesadams@gmail.com>
2015-05-20 08:48:30 -0700

    testing calc_display_score

--- goods/models.py
+++ goods/models.py
@@ -201,12 +201,16 @@ class Product(models.Model):
             hours_since_pub = self.hours_since_pub()
             if hours_since_pub:
                 if hours_since_pub < 24:
+                    print ('less than 24hrs!')
                     pub_dt_score = 40
                 elif hours_since_pub < 72:
+                    print ('less than 72hrs!')
                     pub_dt_score = 20
                 elif hours_since_pub < 120:
+                    print ('less than 120hrs!')
                     pub_dt_score = 5
                 elif hours_since_pub < 336:
+                    print ('less than 336hrs!')
                     pub_dt_score = 3
         except:
             print('pub_date exception')
